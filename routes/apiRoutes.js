3d43a0c9e069c74b6e8a749bed8eda55740601c2
brittanieb <brittanie.boyko@gmail.com>
2020-02-14 00:11:01 -0500

    assigned each note with id corresponding to index in the notes json array - added delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,17 @@
-  
 const notes = require("../db/db");
 const fs = require("fs");
 const util = require("util")
 const path = require("path")
 
 const writeFileAsync = util.promisify(fs.writeFile);
+
+const assignId = (array) => {
+  let idCounter = 0;
+  array.forEach(element => {
+    element.id = idCounter++
+  });
+}
+
 
 module.exports = function(app) {
 
@@ -15,10 +22,22 @@ module.exports = function(app) {
   app.post("/api/notes", function(req, res) {
     let note = req.body
     notes.push(note);
+    assignId(notes);
   
     writeFileAsync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes))
     .then(() => {
       res.json(note);
+    })
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    let id = req.params.id;
+    notes.splice(id, 1);
+    assignId(notes);
+
+    writeFileAsync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes))
+    .then(() => {
+      res.json(id);
     })
   });
 };
